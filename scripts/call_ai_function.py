2e8c59b08d963489a8c617b0abf31184791ff358
InvalidAdmin <121235025+InvalidAdmin@users.noreply.github.com>
2023-04-24 00:16:53 -0600

    Added 'from transformers' and a new 'def call_ai_function'

--- scripts/call_ai_function.py
+++ scripts/call_ai_function.py
@@ -1,7 +1,37 @@
 from config import Config
+from transformers import AutoModelForCausalLM, AutoTokenizer
 
 cfg = Config()
 
+
+def call_ai_function(function, args, description, model_path=None):
+    """Call an AI function"""
+    if model_path is None:
+        model_path = cfg.smart_llm_model
+    tokenizer = AutoTokenizer.from_pretrained(model_path)
+    model = AutoModelForCausalLM.from_pretrained(model_path)
+    # For each arg, if any are None, convert to "None":
+    args = [str(arg) if arg is not None else "None" for arg in args]
+    # parse args to comma seperated string
+    args = ", ".join(args)
+    messages = [
+        {
+            "role": "system",
+            "content": f"You are now the following python function: ```# {description}\n{function}```\n\nOnly respond with your `return` value.",
+        },
+        {"role": "user", "content": args},
+    ]
+
+    response = create_chat_completion(
+        model=model, tokenizer=tokenizer, messages=messages, temperature=0
+    )
+
+    return response
+
+
+
+
+"""
 from llm_utils import create_chat_completion
 # This is a magic function that can do anything with no-code. See
 # https://github.com/Torantulino/AI-Functions for more info.
@@ -26,3 +56,4 @@ def call_ai_function(function, args, description, model=None):
     )
 
     return response
+"""
